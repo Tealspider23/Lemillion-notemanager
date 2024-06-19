522adebf48d4182dc9c839462c0fb56d8694ffad
Tealspider23 <amitbehera204@gmail.com>
2024-06-19 11:06:11 +0530

    chore: Update navigation component with sidebar resizing functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ChevronsLeft } from "lucide-react";
+import { ChevronsLeft, MenuIcon, Sidebar } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useRef, useState } from "react";
 import {useMediaQuery} from 'usehooks-ts';
@@ -18,6 +18,30 @@ const Navigation = () => {
     const [isResetting, setIsResetting] = useState(false);
     const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
+    const handleMouseDown = (
+        event:React.MouseEvent<HTMLDivElement , MouseEvent>
+    )=>{
+        event.preventDefault();
+        event.stopPropagation();
+
+        isResizingRef.current = true;
+        document.addEventListener('mousemove',handleMouseMove);
+        document.addEventListener('mouseup',handleMouseUp);
+
+    }
+
+    const handleMouseMove = (event:MouseEvent) =>{
+        if(!isResizingRef.current) return;
+        let newWidth = event.clientX;
+
+        if(newWidth < 240 ) newWidth=240;
+        if(newWidth> 400 )   newWidth = 480;
+
+        if(sidebarRef.current && navbarRef.current){
+            sidebarRef.current.style.width = `${newWidth}`
+        }
+     }
+
 
 
     return ( 
@@ -30,7 +54,7 @@ const Navigation = () => {
 
         <div
         role='button'
-        className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition">
+        className={cn("h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",isMobile && 'opacity-100')}>
             <ChevronsLeft className="h-6 w-6"/>
         </div>
         <div>
@@ -39,8 +63,19 @@ const Navigation = () => {
         <div className="mt-4">
             <p>Documents</p>
         </div>
-        <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize  absolute h-full w-1 bg-primary/10 right-0 top-0" />
+        <div
+        onMouseDown={()=>{}}
+        onClick={()=>{}} 
+        className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize  absolute h-full w-1 bg-primary/10 right-0 top-0" />
         </aside>
+        <div
+            ref = {navbarRef}
+            className={cn("absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",isResetting && 'transition-all ease-in-out duration-300',isMobile && 'left-0 w-full')}
+        >
+            <nav className="bg-transparent px-3 py-2 w-full">
+                {isCollapsed && <MenuIcon role='button' className="h-6 w-6 text-muted-foreground " />}
+            </nav>
+        </div>
         </>
      );
 }
